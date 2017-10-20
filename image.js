4714305759a1c6410b1dc08243da114bd9439fba
galen <827472476@qq.com>
2017-10-20 15:41:18 +0800

    image.js

--- image.js
+++ image.js
@@ -9,3 +9,11 @@ require('http').createServer(function(req,res) {
     res.end();
   });
 }).listen(3000);
+
+//实际上是： 流(stream)文件系统(piping)------流(http.ServerResponse对象)的过程
+//简化版
+require('http').createServer(function(req,res) {
+  res.writeHead(200,{'Content-Type':'image/png'});
+  //通过管道到response中
+  require('fs').createReadStream('image.png').pipe(res);
+}).listen(3000);
